120ab882402ea371a2f62a5ab1ac761306d01130
Igor-Filin <Igor.Filin@smartbear.com>
2015-07-06 10:43:04 +0300

    Added TestComplete/TestExecute 11 version selection capability

--- src/main/java/com/smartbear/jenkins/plugins/testcomplete/TcTestBuilder.java
+++ src/main/java/com/smartbear/jenkins/plugins/testcomplete/TcTestBuilder.java
@@ -803,6 +803,7 @@ public class TcTestBuilder extends Builder implements Serializable {
         public ListBoxModel doFillExecutorVersionItems() {
             ListBoxModel model = new ListBoxModel();
             model.add(Messages.TcTestBuilder_Descriptor_LatestTagText(), Constants.ANY_CONSTANT);
+            model.add("11", "11.0");
             model.add("10", "10.0");
             model.add("9", "9.0");
             return model;
